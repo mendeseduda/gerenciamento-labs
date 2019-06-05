bf16442a8a959c08ed608e3deca72f00f5ff77a9
Eduardo Mendes <du-du7@hotmail.com>
2019-06-04 22:26:50 -0300

    Iniciando o projeto

---
+++ views/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <?php
+        include("./layout/head.php");
+        include("./layout/body.php");
+    ?>
+</html>
+
+
+
---
+++ views/login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include("./layout/head.php");
+?>
+<body>
+    
+</body>
+</html>
