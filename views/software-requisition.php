9cf99222087dd1a76f1c563c1b339e63f5a6604a
jouling <jujuhipolito17@gmail.com>
2019-06-18 22:24:22 -0300

    Iniciando a implementação da requisição de softwares

---
+++ views/software-requisition.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include(__DIR__ . "/layout/head.php")?>
+<body>
+<?php include(__DIR__ . "/layout/navBar.php")?>
+<form>
+  <div class="form-group col-sm">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group col-sm">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check check-me-out">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary check-me-out">Submit</button>
+</form>
+</body>
+</html>
+
