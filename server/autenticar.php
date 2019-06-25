cd100de26a117c93cd7b3a02dda13e58e85b806c
carinolas <38460877+carinolas@users.noreply.github.com>
2019-06-24 21:18:09 -0300

    Consertando erros em login

--- server/autenticar.php
+++ server/autenticar.php
@@ -7,16 +7,14 @@ $senha = $_POST['senha'];
 $fp = fopen("../data/users.csv", "r");
 $usuarioEncontrado = false;
 
-$contador = 0;
-
 while(!feof($fp)){
-    echo fgetcsv($fp)[$contador][2].fgetcsv($fp)[$contador][3];
-    if(fgetcsv($fp)[$contador][2] == $email && fgetcsv($fp)[$contador][3] == $senha){
+    $campos = fgetcsv($fp);
+    
+    if($campos[2] == $email && $campos[3] == $senha){
         $usuarioEncontrado = true;
-        }
-        $contador++;
+        break;
     }
-
+}
     if($usuarioEncontrado) {
         $_SESSION["usuario"] = $email;
         header('Location: http://localhost/gerenciamento-labs/views/index.php');
